69339fedbbd4322b42627919caa0922ce2a35b62
Kaua-Melchior <134136996+Kaua-Melchior@users.noreply.github.com>
2024-03-06 17:41:26 -0300

    V.1.1

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-E.speaker.beep()
+ev3 = EV3Brick()
+
+ev3.speaker.beep()
+
 
 #Definiçoes das variaveis do PID
-Speed = 22
+Speed = 55
 Kp = 2
 Kd = 2
 Limite = 20
 Derivada=Proporcional=Error=Prev_Error=Pd=Speed1=Speed2 = 0
 
 # Iniciação dos sensores de cor nas portas 1 a 4
-Sensor_Direita = ColorSensor(Port.S1)
-Sensor_Esquerda = ColorSensor(Port.S2)
+Sensor_Direita = ColorSensor(Port.S2)
+Sensor_Esquerda = ColorSensor(Port.S1)
 #Sensor_Frente = ColorSensor(Port.S3)
 #Sensor_UltraSonico = UltrasonicSensor(Port.S4)
 
@@ -28,12 +30,19 @@ Motor_Direito = Motor(Port.B)
 #Motor_Caçamba = Motor(Port.C)
 #Motor_Garra = Motor(Port.D)
 
-# Função PID
-def PID():
-    intensidade_Direita = sensor_direita.reflection()
+#Função Reflection
+def dados():
+    intensidade_Direita = Sensor_Direita.reflection()
     intensidade_Esquerda = Sensor_Esquerda.reflection()
 
-    Error = intensidade_porta1 - intensidade_porta2
+    ev3.screen.print(intensidade_Esquerda)
+
+# Função PID
+def PID():
+    intensidade_Direita = Sensor_Direita.reflection()
+    intensidade_Esquerda = Sensor_Esquerda.reflection()
+
+    Error = intensidade_Direita - intensidade_Esquerda
     Proporcional = Error * Kp
     Derivada = (Error - Prev_Error) * Kd
     Pd = Proporcional - Derivada
@@ -46,12 +55,12 @@ def PID():
 
     Motor_Esquerdo.dc(Speed1)
     Motor_Direito.dc(Speed2)
-    
+    wait(5)
     Motor_Esquerdo.stop()
     Motor_Direito.stop()
-    return diferenca
+    
 
 # Exemplo de utilização da função
-resultado = diferenca_intensidade()
-print("Diferença na intensidade de luz:", resultado)
+while True:
+    dados()
 
